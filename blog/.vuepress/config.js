66c2a07826f051fed00626918e0a1cce42e98110
natandias <natandias@pm.me>
2021-05-14 12:01:11 -0300

    Feat: add favicon

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -1,6 +1,16 @@
 module.exports = {
   title: 'Natan Dias',
   theme: '@vuepress/theme-blog',
+  description: 'Algumas anotações sobre tecnologia',
+  head: [
+    ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/assets/favicons/apple-touch-icon.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/assets/favicons/favicon-32x32.png"}],
+    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/assets/favicons/favicon-16x16.png"}],
+    ['link', { rel: "manifest", href: "/assets/favicons/site.webmanifest"}],
+    ['link', { rel: "shortcut icon", href: "/assets/favicons/favicon.ico"}],
+    ['meta', { name: "msapplication-TileColor", content: "#51616b"}],
+    ['meta', { name: "theme-color", content: "#ffffff"}],
+  ],
   plugins: ['vuepress-plugin-seo', '@vuepress/back-to-top', '@vuepress/medium-zoom'],
   shouldPrefetch: () => false,
   base: "/myblog/",
